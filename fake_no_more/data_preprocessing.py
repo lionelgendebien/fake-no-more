ca189ffcc29830a7e296388f9589b5b76cbb1620
Mejrima Suljkanovic <mejrimasljk@gmail.com>
2024-12-05 12:02:55 +0100

    processed update

--- fake_no_more/data_preprocessing.py
+++ fake_no_more/data_preprocessing.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import os
 import math
+import pickle
+import numpy as np
 
 # Create X and y
 def create_features_and_target(df):
@@ -19,64 +21,51 @@ def y_encoding(y):
     encoder = LabelEncoder()
     return encoder.fit_transform(y.values.ravel())
 
-# Train-test split
-def split_train_test(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
-    return X_train, X_test, y_train, y_test
-
-def split_train_test_LSTM(X, y):
+def reshaping_X_LSTM(X,y):
     time_steps = 157  # Number of rows per audio file
-    print(type(time_steps))  # This will print <class 'int'>
-    print(len(X))
     num_audio = len(X) // time_steps
-    print(type(time_steps))# Total number of audio files
     features = X.shape[1]# Number of features per row
-    print(type(features))
+    X_reshaped = X.to_numpy().reshape(num_audio, time_steps, features)
+    y_reshaped = y.reshape(-1, time_steps)[:, 0]
+    return X_reshaped, y_reshaped
+
+
+# Train-test split
+def split_train_test_LSTM(X,X_reshaped, y_reshaped):
+    time_steps = 157  # Number of rows per audio file
+    num_audio = len(X) // time_steps
     test_size = math.ceil(num_audio * 0.2)
-    print(test_size)
-    X_reshaped = X.reshape(num_audio, time_steps, features)
-    y_grouped = y.reshape(-1, time_steps)[:, 0]  # Take the first label for each file
-
-    X_train, X_test, y_train, y_test = train_test_split(X_reshaped, y_grouped, test_size = test_size, random_state=42, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(X_reshaped, y_reshaped, test_size = test_size, random_state=42, shuffle=True)
     return X_train, X_test, y_train, y_test
-
 
 # Train-val split
-def split_train_val(X_train, y_train):
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.20)
-    return X_train, X_val, y_train, y_val
-
 def split_train_val_LSTM(X_train, y_train):
     test_size = math.floor(X_train.shape[0]*0.2)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = test_size, random_state=42, shuffle=True)
     return X_train, X_val, y_train, y_val
 
 # Feature scaling
-def min_max_scaler(X_train, X_test, X_val):
-    scaler = MinMaxScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    X_val_scaled = scaler.transform(X_val)
-    return X_train_scaled, X_test_scaled, X_val_scaled
+# def min_max_scaler_LSTM(X_train, X_test, X_val):
+#     scaler = MinMaxScaler()  # Create the scaler
+#     scaler.fit(X_train)  # Fit the scaler on the training data
+#     scalerfile = 'scaler_fitted.sav'
+#     pickle.dump(scaler, open(scalerfile, 'wb'))  # Save the fitted scaler
 
-def min_max_scaler_LSTM(X):
-    scaler = MinMaxScaler()
-    X_scaled = scaler.fit_transform(X)
-    return X_scaled
+#     # Transform the data
+#     X_train_scaled = scaler.transform(X_train)
+#     X_test_scaled = scaler.transform(X_test)
+#     X_val_scaled = scaler.transform(X_val)
+
+#     return X_train_scaled, X_test_scaled, X_val_scaled
+
+
 
 # Preprocess data
-def process_data(df):
-    X, y =create_features_and_target(df)
-    y = y_encoding(y)
-    X_train, X_test, y_train, y_test = split_train_test(X,y)
-    X_train, X_val, y_train, y_val = split_train_val(X_train,y_train)
-    X_train_scaled, X_test_scaled, X_val_scaled = min_max_scaler(X_train, X_test, X_val)
-    return X_train_scaled, X_test_scaled, X_val_scaled, y_train, y_test, y_val
-
 def process_data_LSTM(df):
     X, y =create_features_and_target(df)
     y = y_encoding(y)
-    X_scaled = min_max_scaler_LSTM(X)
-    X_train_scaled, X_test_scaled, y_train, y_test = split_train_test_LSTM(X_scaled,y)
-    X_train_scaled, X_val_scaled, y_train, y_val = split_train_val_LSTM(X_train_scaled,y_train)
-    return X_train_scaled, X_test_scaled, X_val_scaled, y_train, y_test, y_val
+    X_reshaped, y_reshaped = reshaping_X_LSTM(X,y)
+    X_train, X_test, y_train, y_test = split_train_test_LSTM(X, X_reshaped,y_reshaped)
+    X_train, X_val, y_train, y_val = split_train_val_LSTM(X_train,y_train)
+    # X_train_scaled, X_test_scaled, X_val_scaled=min_max_scaler_LSTM(X_train, X_test, X_val)
+    return X_train, X_test, X_val, y_train,y_test, y_val
